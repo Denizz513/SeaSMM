042ae56fe374b5506a870c16cc345374e09eb0e0
Denizz513 <sea513@hotmail.com>
2025-07-21 11:27:13 +0300

    Update bakim_durum.py

--- commands/bakim_durum.py
+++ commands/bakim_durum.py
@@ -1,17 +1,16 @@
 import discord
 from discord.ext import commands
 from discord import app_commands
-from helpers.state import get_bakim_modu
-from config import ADMIN_IDS
+from utils import db
 
 class BakimDurum(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @app_commands.command(name="bakim-durum", description="Botun bakımda olup olmadığını gösterir.")
+    @app_commands.command(name="bakim-durum", description="Bakım modunun açık mı kapalı mı olduğunu gösterir.")
     async def bakim_durum(self, interaction: discord.Interaction):
         await interaction.response.defer(ephemeral=True)
-        durum = "✅ Kapalı" if not get_bakim_modu() else "⚠️ Açık"
+        durum = "✅ Kapalı" if not db.bakim_modu else "⚠️ Açık"
         await interaction.followup.send(f"🔧 Bakım modu durumu: {durum}")
 
 async def setup(bot):
